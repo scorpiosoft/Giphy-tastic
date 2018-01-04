bf403f48f03a63f92c0801785d7e17c7aa164d35
Christopher Biessener <cpbiessener@gmail.com>
2018-01-04 10:52:36 -0600

    display stills

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -5,6 +5,8 @@ var Giphytastic =
   d_gifs:       $("#gifs"),
   // array of button names
   buttons: ["Star Trek", "Star Wars", "The Matrix", "The Expanse", "Continuum"],
+  // GIF limit
+  limit: 10,
   // method to add a button
   add_button: function ()
   {
@@ -16,7 +18,7 @@ var Giphytastic =
   {
     var button;
 
-    button = $('<button class="btn btn-success float-left mr-2">'+button+'</button>');
+    button = $('<button class="btn btn-success subject float-left mr-2">'+button+'</button>');
     this.d_buttons.append(button);
   },
   // method to display all buttons
@@ -28,6 +30,34 @@ var Giphytastic =
       this.display_button(this.buttons[i]);
     }
   },
+  // method to get GIFs of the given subject
+  random_gifs: function (subject)
+  {
+    // clear the display
+    Giphytastic.d_gifs.empty();
+    // query Giphy for GIFs
+    $.ajax(
+    {
+      url: "https://api.giphy.com/v1/gifs/search?api_key=2NSUkoto46BJKC6mgko4RKD1vqgESOCX&q="+subject+"&limit="+Giphytastic.limit,
+      method: 'GET'
+    }).done(function(response)
+    {
+      console.log(response);
+
+      for (var i = 0; i < Giphytastic.limit; ++i)
+      {
+        gif = $('<img>',
+          {
+            id:    'gif'+i,
+            class: 'giphy',
+            src:   response.data[i].images.fixed_width_still.url,
+          });
+        gif.css('margin', '0 20px 20px 0');
+        Giphytastic.d_gifs.append(gif);
+      }
+    });
+
+  },
 };
 
 //
@@ -38,6 +68,12 @@ Giphytastic.display_all_buttons();
 //
 // Event Functions
 //
+
+// Click function for the start buttons
+Giphytastic.d_buttons.on('click', 'button.subject', function()
+{
+  Giphytastic.random_gifs($(this).text());
+});
 
 //
 // Utility Functions
